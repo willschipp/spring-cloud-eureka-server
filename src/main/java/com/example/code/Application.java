6a67d649e3354c81a1cef2db09f0d01b9a4ced07
Will Schipp <3287360+willschipp@users.noreply.github.com>
2023-07-26 21:38:21 -0500

    WIP: client-only

--- src/main/java/com/example/code/Application.java
+++ src/main/java/com/example/code/Application.java
@@ -2,8 +2,6 @@ package com.example.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,9 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication(proxyBeanMethods = false)
-@EnableEurekaServer
 @EnableAdminServer
-@EnableDiscoveryClient
 public class Application {
 
     public static void main(String... args) throws Exception {
