21f86207abbbc285e24311287d652c0b9d65f20a
Will Schipp <3287360+willschipp@users.noreply.github.com>
2023-07-26 22:46:07 -0500

    WIP: update based on k8s discovery

--- src/main/java/com/example/code/Application.java
+++ src/main/java/com/example/code/Application.java
@@ -2,14 +2,18 @@ package com.example.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication(proxyBeanMethods = false)
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableScheduling
 public class Application {
 
     public static void main(String... args) throws Exception {
